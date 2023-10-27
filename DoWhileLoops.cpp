d8c2560ad7b1677b0fd76fa1bc4e438766641664
Blacky <97872836+DrBlack22@users.noreply.github.com>
2023-10-27 11:21:45 +0500

    First commit

---
+++ DoWhileLoops.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main ()
+{
+    int x = 1;
+    do{
+         cout << x << endl;
+         x++;
+    }while(x<10);
+}
+
+/* DO WHILE LOOP
+
+- Runs the code first and test later.
+
+- Runs at least ONE time no matter the value, runs the test after that.
+
+*/
